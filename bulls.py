e0fe1c83f03b03c9359e6f92ebca048f656d1aee
sanaasraf <118794664+sanaasraf@users.noreply.github.com>
2022-12-07 02:31:03 +0400

    Changed file #5

--- bulls.py
+++ bulls.py
@@ -1,13 +1,19 @@
 import random
 
 def compare_numbers(number, user_guess):
-    ## your code here
-    return cowbull
+   cowbull = [0, 0]
+   for i in range(len(number)):
+        if number[i] == user_guess[i]:
+            cowbull[1] +=1
+        else: 
+            cowbull[0]+=1
+        return cowbull
+
 
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +22,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!")
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
